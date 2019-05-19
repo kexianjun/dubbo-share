e9b0ad84175a5d24a1ffbbac319953cfca6cf735
kexianjun <kexianjun1991@hotmail.com>
2019-05-19 10:06:19 +0800

    多线程环境下调用 DefaultFuture.get() 方法阻塞

--- common-consumer/src/main/java/dubbo/test/common/consumer/service/ConsumerServiceDemo.java
+++ common-consumer/src/main/java/dubbo/test/common/consumer/service/ConsumerServiceDemo.java
@@ -1,27 +1,58 @@
 package dubbo.test.common.consumer.service;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.dubbo.rpc.RpcContext;
 import dubbo.test.api.DemoService;
 import dubbo.test.api.DemoServiceB;
 import dubbo.test.domain.Student;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 @Component
 public class ConsumerServiceDemo {
 
-    @Reference(check = false, interfaceName = "demoService")
+    @Reference(check = false, async = true, timeout = 10000)
     private DemoService demoService;
-
-    @Reference(check = false, interfaceName = "demoService")
-    private DemoServiceB demoServiceB;
 
     public void demoServiceTest() {
         Student student = new Student();
         student.setSchool("xxx");
         student.setName("XXXXXX");
+        demoService.sayHello(student);
+        Future<Object> future = RpcContext.getContext().getFuture();
+        Thread thread1 = new Thread(() -> {
+            System.out.println("thead 1 run begin ," + System.currentTimeMillis());
+            Object o = null;
+            try {
+                o = future.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            System.out.println("thread 1 get result:" + o + ", " + System.currentTimeMillis());
 
-        System.out.println("from remoting: " + demoService.sayHello(student));
-        System.out.println("from demo service B:" + demoServiceB.demoServiceBTest("hello from B"));
+        });
+
+
+        Thread thread2 = new Thread(() -> {
+            System.out.println("thead 2 run begin ," + System.currentTimeMillis());
+            Object o = null;
+            try {
+                o = future.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            System.out.println("thread 2 get result:" + o + ", " + System.currentTimeMillis());
+
+        });
+
+        thread1.start();
+        thread2.start();
 
     }
 }
